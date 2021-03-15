e4a6d87f4c9bdaf4ca838201ace45f1d6fc5ee5e
rgguriev <60221719+rgguriev@users.noreply.github.com>
2021-03-16 00:17:11 +0100

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let res = [];
+  let conf = bracketsConfig;
+  // use cycle for or forEach+join()
+  for (let i = 0; i < conf.length; i++) {
+    res.push(conf[i][0] + conf[i][1]);
+  }
+  // remove brackets from res
+  for (let j = 0; j < str.length; j++) {
+    if (str.includes(res[j])) {
+      str = str.replace(res[j], "");
+      j = -99;
+    }
+  }
+  return !str;
 }
